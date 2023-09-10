021b7e7cc555bab1d5a366df0dd6ba90b0e510b7
hariharan2004 <hari2004haran@gmail.com>
2023-09-10 07:21:46 +0530

    Header and Sidebar

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import loclogo from '../assets/location.png';
+
+const Header = (props) => {
+    const current_location = "Potheri, Chengalpattu";
+    const imgw = '50px'; 
+    const imgh = '50px';
+    return (
+        <div className='header1'>
+            <div className='header-content'>
+                <div className='heading1'>
+                    <h1>RES-Q</h1>
+                </div>
+                <div className='location'>
+                    <div className="location-text">
+                        <h1>{current_location}</h1>
+                    </div>
+                    <div className="location-image">
+                        <img src={loclogo} alt="Current Location" style={{ width: imgw, height: imgh }} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Main from './components/main/main';
-
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Rescue from './components/Rescue';
+import './styles/styles.css';
+import './styles/header.css';
+import './styles/sidebar.css';
+import './styles/rescue.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Main />
+  <div className='maindiv'>
+    <Header />
+    <Sidebar />
+  </div>
 );
